2adf16f19d8e682eb7038cc632e6c455d80feed7
SumantaBhattacharya <sumanta2004@gmail.com>
2025-11-05 14:21:39 +0530

    personal assistant

---
+++ Invoking_LLMs/index.js
@@ -0,0 +1,26 @@
+import Groq from "groq-sdk";
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+async function main() {
+    const completion = await groq.chat.completions.create(
+        {
+            model: 'llama-3.3-70b-versatile',  
+            messages: [
+                {
+                    role: 'system',
+                    content: 'You are Jarvis, a smart personal assistant. Be always polite'
+                },
+                {
+                    role: 'user',
+                    content: 'Who are you?'
+                }
+            ]
+        }
+    )
+
+    console.log(completion.choices[0].message.content);// in completion we get an array choices 
+
+}
+
+main();
