de84517b3b880bd7a976346ece656a2b439a4b78
SumantaBhattacharya <sumanta2004@gmail.com>
2025-11-15 09:36:08 +0530

    Ignored chatGPT/node_modules

--- URL-Shortner/backend/src/routes/url_shortner.route.js
+++ URL-Shortner/backend/src/routes/url_shortner.route.js
@@ -5,7 +5,7 @@ import {authMiddlewere} from '../middlewere/auth.middlewere.js';
 const router = express.Router();
 
 router.post("/create", authMiddlewere, createShortUrl);// http://localhost:4001/api/kt9D0fM
-router.get("/:short_url", RedirectUrl);// 
+router.get("/:short_url", RedirectUrl);// id
 
 router.post("/create-custom-url", authMiddlewere, createCustomShortUrl);// req.user is only set if your auth middleware runs before the controller and validates a JWT token (usually from Authorization header or a cookie).
 
